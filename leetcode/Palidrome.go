a9ab6302a70291847ca25380bba7302e6835d51b
Fokion Sotiropoulos <fokion.s@gmail.com>
2022-02-14 21:06:50 +0000

    fix tests and checking my solution

--- leetcode/Palidrome.go
+++ leetcode/Palidrome.go
@@ -1,13 +1,34 @@
 package leetcode
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 )
 
 func IsPalidrome(text string) bool {
 	text = GetCleanText(text)
-	return false
+	if len(text) <= 1 {
+		return true
+	}
+	//divide text in half
+	index := len(text) / 2
+	firstHalf := text[:index]
+	secondHalf := text[index:]
+	if len(text)%2 == 1 {
+		firstHalf = text[:(index + 1)]
+		secondHalf = text[index+1:]
+	}
+	length := len(firstHalf)
+	fmt.Println(firstHalf, secondHalf, length)
+	for i := 0; i < length; i++ {
+		fmt.Println(string(firstHalf[i]), string(secondHalf[(length-i-1)]))
+		if !strings.EqualFold(string(firstHalf[i]), string(secondHalf[(length-i-1)])) {
+			return false
+		}
+	}
+
+	return true
 }
 
 func GetCleanText(text string) string {
--- leetcode/Palidrome_test.go
+++ leetcode/Palidrome_test.go
@@ -12,13 +12,13 @@ func TestIsPalidrome_anna(t *testing.T) {
 }
 
 func TestIsPalidrome_test(t *testing.T) {
-	if !IsPalidrome("test") {
+	if IsPalidrome("test") {
 		t.Errorf("test is not a palidrome")
 	}
 }
 
 func TestIsPalidrome_palidrome_with_spaces_and_other_symbols(t *testing.T) {
-	if IsPalidrome("race $123^ ecar") {
+	if !IsPalidrome("race $123^ ecar") {
 		t.Errorf("raceecar is a palidrome")
 	}
 }
